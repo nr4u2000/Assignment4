f3d6cbffdf2cff087a863353fc7e69ef86cdeb81
Niel <89479787+nr4u2000@users.noreply.github.com>
2026-03-28 21:24:16 +0530

    Card Flip logic implemented

---
+++ Assets/Scripts/Card.cs
@@ -0,0 +1,77 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+public class Card : MonoBehaviour
+{
+    public int Id;
+    public bool IsMatched { get; private set; }
+
+    [SerializeField] private GameObject front;
+    [SerializeField] private GameObject back;
+    [SerializeField] private Text ValueTextfield;
+
+    private bool isFlipped = false;
+    private bool isAnimating = false;
+
+    public void Start()
+    {
+        ValueTextfield.text = Id.ToString();
+
+    }
+
+    public void OnClick()
+    {
+        if (isFlipped || IsMatched || isAnimating) return;
+
+        Flip();
+    }
+
+    public void Flip()
+    {
+        StartCoroutine(FlipRoutine(true));
+    }
+
+    public void FlipBack()
+    {
+        StartCoroutine(FlipRoutine(false));
+    }
+
+    IEnumerator FlipRoutine(bool showFront)
+    {
+        isAnimating = true;
+
+        float duration = 0.2f;
+        float time = 0;
+
+        while (time < duration)
+        {
+            float scale = Mathf.Lerp(1, 0, time / duration);
+            transform.localScale = new Vector3(scale, 1, 1);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        front.SetActive(showFront);
+        back.SetActive(!showFront);
+
+        time = 0;
+        while (time < duration)
+        {
+            float scale = Mathf.Lerp(0, 1, time / duration);
+            transform.localScale = new Vector3(scale, 1, 1);
+            time += Time.deltaTime;
+            yield return null;
+        }
+        transform.localScale = Vector3.one;
+
+        isFlipped = showFront;
+        isAnimating = false;
+    }
+
+    public void SetMatched()
+    {
+        IsMatched = true;
+    }
+}
